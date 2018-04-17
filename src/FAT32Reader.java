d19ed9d05ea99d053904220c46e48973bd0cf063
Avi Amon <aviamon11@gmail.com>
2018-04-16 22:50:42 -0400

    Added convertHexToString in fat32reader

--- src/FAT32Reader.java
+++ src/FAT32Reader.java
@@ -42,4 +42,30 @@ public class FAT32Reader {
         }
         return withLeadingZeros.substring(i);
     }
+
+    /**
+     * Converts Hex values to Strings
+     * Taken from https://www.mkyong.com/java/how-to-convert-hex-to-ascii-in-java/
+     * @param hex The hex to be converted
+     * @return The String representation of the hex
+     */
+    public String convertHexToString(String hex){
+        StringBuilder sb = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+
+        for( int i=0; i<hex.length()-1; i+=2 ){
+
+            //grab the hex in pairs
+            String output = hex.substring(i, (i + 2));
+            //convert hex to decimal
+            int decimal = Integer.parseInt(output, 16);
+            //convert the decimal to character
+            sb.append((char)decimal);
+
+            temp.append(decimal);
+        }
+        System.out.println("Decimal : " + temp.toString());
+
+        return sb.toString();
+    }
 }
